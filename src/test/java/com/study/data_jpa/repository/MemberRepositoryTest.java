91ad6023ccf6706800a71154b2ff7e45eb93e124
alwaysJOne <alwaysjone@naver.com>
2024-12-02 17:44:01 +0900

    init

---
+++ src/test/java/com/study/data_jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.study.data_jpa.repository;
+
+import com.study.data_jpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+
+        Member member = new Member("memberA");
+        Member saveMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(saveMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(saveMember.getUsername());
+        assertThat(saveMember).isEqualTo(findMember);
+    }
+}
